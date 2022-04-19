d5ae841223abbf71fcf9246886af0aa46e1eef3c
Nicolai Parlog <nipa@codefx.org>
2022-04-19 07:46:07 +0200

    Create preliminary modules

---
+++ eclipse-collections-forkjoin/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.eclipse.collections.forkjoin {
+    requires transitive org.eclipse.collections.api;
+    requires transitive org.eclipse.collections;
+
+    exports org.eclipse.collections.impl.forkjoin;
+}
---
+++ eclipse-collections-testutils/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module eclipse.collections.testutils {
+    requires transitive org.eclipse.collections.api;
+    requires org.eclipse.collections;
+
+    requires junit;
+
+    exports org.eclipse.collections.impl.test;
+}
